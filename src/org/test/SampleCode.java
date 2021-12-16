c437dc612fbe8f6db5d049d8ba67e0e727e13ccb
NavinaV23 <navinavbtechit>
2021-12-16 14:38:48 +0530

    done

---
+++ src/org/test/SampleCode.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class SampleCode {
+public static void main(String[] args) {
+	
+	System.out.println("sun");
+	System.out.println("mercury");
+	System.out.println("venus");
+	
+}
+	
+	
+	
+}
